0e15a26489666c7cee2b8df9bac19e3565a4577a
Justin <justin@tinylan.com>
2023-06-17 23:25:31 -0400

    First!

---
+++ src/components/ThemeSwitcher.js
@@ -0,0 +1,22 @@
+export class ThemeSwitcher {
+    constructor() {
+        this.setTheme(this.getPreferredTheme());
+    }
+
+    setTheme(theme) {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('theme', 'auto');
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme);
+            localStorage.setItem('theme', theme);
+        }
+    }
+
+    getPreferredTheme() {
+        const storedTheme = localStorage.getItem('theme');
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark').matches;
+        debugger;
+        return storedTheme ?? (prefersDarkMode ? 'dark' : 'light');
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,67 @@
+// Import our custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import { ThemeSwitcher } from '../components/ThemeSwitcher';
+import { RFExposureCalculator } from 'rf-exposure-calculator';
+
+const themeSwitcher = new ThemeSwitcher();
+const themeSelector = document.querySelector('#js-theme-selector');
+
+themeSelector.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const el = event.target.closest('li');
+    const selectedTheme = el.dataset.theme;
+
+    themeSwitcher.setTheme(selectedTheme);
+});
+
+const calculator = new RFExposureCalculator();
+const formElem = document.querySelector('#params');
+
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const powerInWatts = parseFloat(formElem.querySelector('#power').value);
+    const modeDutyCycle = parseFloat(formElem.querySelector('#mode-duty-cycle').value);
+    const txDutyCycle = {
+        tx: parseFloat(formElem.querySelector('#tx-duty-cycle\\[tx\\]').value),
+        rx: parseFloat(formElem.querySelector('#tx-duty-cycle\\[rx\\]').value)
+    };
+    const antennaGainInDbi = parseFloat(formElem.querySelector('#antenna-gain').value);
+    const operatingFrequencyInMhz = parseFloat(formElem.querySelector('#operating-frequency').value);
+    const includeGroundReflectionEffects = formElem.querySelector('#ground-reflection-effects').checked;
+
+    const controlled = calculator.calculateRFExposure(
+        powerInWatts,
+        modeDutyCycle,
+        txDutyCycle,
+        antennaGainInDbi,
+        operatingFrequencyInMhz,
+        true,
+        includeGroundReflectionEffects
+    );
+
+    const uncontrolled = calculator.calculateRFExposure(
+        powerInWatts,
+        modeDutyCycle,
+        txDutyCycle,
+        antennaGainInDbi,
+        operatingFrequencyInMhz,
+        false,
+        includeGroundReflectionEffects
+    );
+
+    const resultsElem = document.querySelector('#results');
+
+    // Controlled
+    resultsElem.querySelector('#controlled-max-allowed-power-density').innerText = controlled.max_allowed_power_density;
+    resultsElem.querySelector('#controlled-min-safe-distance').innerText = controlled.min_safe_distance_in_feet;
+
+    // Uncontrolled
+    resultsElem.querySelector('#uncontrolled-max-allowed-power-density').innerText = uncontrolled.max_allowed_power_density;
+    resultsElem.querySelector('#uncontrolled-min-safe-distance').innerText = uncontrolled.min_safe_distance_in_feet;
+});
